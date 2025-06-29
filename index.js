dfcd623fc5638aab163dbc8f1f14edd7f7795285
ayushm231 <ayushm231@gmail.com>
2025-06-29 14:37:09 +0530

    Fix: use process.env.PORT for Render

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.json())
 const path = require("path")
 app.use(express.static(path.join(__dirname, "public")))
 
+const PORT = process.env.PORT || 5000
+
 // Routes
 app.use("/api/auth", authRoutes)
 app.use("/api/onboarding", onboardingRoutes)
@@ -25,6 +27,6 @@ mongoose
   })
   .then(() => {
     console.log("MongoDB connected")
-    app.listen(5000, () => console.log("Server running on port 5000"))
+    app.listen(PORT, () => console.log("Server running on port 5000"))
   })
   .catch((err) => console.log(err))
